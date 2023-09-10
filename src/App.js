8a6ef5a39f44d8e5e1f98a8e635bf146c61ce66c
Anas Qazza <anasqazza@gmail.com>
2023-09-10 02:19:00 -0700

    finished sidebar and working on team jsx

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import { Routes, Route } from "react-router-dom";
 import Topbar from "./scenes/global/Topbar";
 import Sidebar from "./scenes/global/Sidebar";
 import Dashboard from "./scenes/dashboard/index";
-// import Team from "./scenes/team/Team";
+import Team from "./scenes/team/index";
 // import Invoices from "./scenes/invoices/Invoices";
 // import Clients from "./scenes/clients/Clients";
 // import Bar from "./scenes/charts/Bar";
@@ -28,8 +28,8 @@ function App() {
             <Topbar />
             <Routes>
               <Route path="/" element={<Dashboard />} />
-              {/* <Route path="/team" element={<Team />} />
-              <Route path="/invoices" element={<Invoices />} />
+              <Route path="/team" element={<Team />} />
+              {/* <Route path="/invoices" element={<Invoices />} />
               <Route path="/clients" element={<Clients />} />
               <Route path="/charts/bar" element={<Bar />} />
               <Route path="/charts/line" element={<Line />} />
