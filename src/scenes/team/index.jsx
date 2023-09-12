710a89fa1ffd7b49f841b6e257ef49dd95b01ac7
Anas Qazza <anasqazza@gmail.com>
2023-09-12 01:03:49 -0700

    Added a form to manage users to alloq additions to the team

--- src/scenes/team/index.jsx
+++ src/scenes/team/index.jsx
@@ -1,4 +1,14 @@
-import { Box, Typography, useTheme } from "@mui/material";
+import {
+  Box,
+  Typography,
+  useTheme,
+  TextField,
+  Button,
+  FormControl,
+  InputLabel,
+  Select,
+  MenuItem,
+} from "@mui/material";
 import { tokens } from "../../theme";
 import { DataGrid } from "@mui/x-data-grid";
 import { mockDataTeam } from "../../data/mockData";
@@ -6,10 +16,20 @@ import AdminPanelSettingsOutlined from "@mui/icons-material/AdminPanelSettingsOu
 import LockOpenOutlined from "@mui/icons-material/LockOpenOutlined";
 import SecurityOutlined from "@mui/icons-material/SecurityOutlined";
 import Header from "../../components/Header";
+import { useState } from "react";
 
 const Team = () => {
   const theme = useTheme();
   const colors = tokens(theme.palette.mode);
+
+  const [name, setName] = useState("");
+  const [age, setAge] = useState("");
+  const [id, setId] = useState("");
+  const [email, setEmail] = useState("");
+  const [access, setAccess] = useState("");
+
+  // State variable for showing/hiding the form
+  const [isFormVisible, setFormVisibility] = useState(false);
 
   const columns = [
     { field: "id", headerName: "ID" },
@@ -59,6 +79,22 @@ const Team = () => {
       },
     },
   ];
+  // Function to handle form submission
+  const handleAddUser = () => {
+    // Perform actions to add the user with the provided information
+    // You can use the state variables (name, age, id, email, access) here
+    // Then, clear the form and hide it
+    clearForm();
+    setFormVisibility(false);
+  };
+  // Function to clear the form fields
+  const clearForm = () => {
+    setName("");
+    setAge("");
+    setId("");
+    setEmail("");
+    setAccess("");
+  };
   return (
     <Box m="20px">
       <Header
@@ -94,6 +130,71 @@ const Team = () => {
           },
         }}
       >
+        <Button
+          variant="contained"
+          color="primary"
+          onClick={() => setFormVisibility(!isFormVisible)}
+        >
+          {isFormVisible ? "Close Form" : "Add User"}
+        </Button>
+        {isFormVisible && (
+          <form onSubmit={handleAddUser}>
+            <TextField
+              label="Name"
+              variant="outlined"
+              fullWidth
+              value={name}
+              onChange={(e) => setName(e.target.value)}
+              required
+            />
+            <TextField
+              label="Age"
+              variant="outlined"
+              fullWidth
+              type="number"
+              value={age}
+              onChange={(e) => setAge(e.target.value)}
+              required
+            />
+
+            <TextField
+              label="ID"
+              variant="outlined"
+              fullWidth
+              value={id}
+              onChange={(e) => setId(e.target.value)}
+              required
+            />
+
+            <TextField
+              label="Email"
+              variant="outlined"
+              fullWidth
+              type="email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+              required
+            />
+
+            <FormControl fullWidth variant="outlined">
+              <InputLabel>Access Level</InputLabel>
+              <Select
+                label="Access Level"
+                value={access}
+                onChange={(e) => setAccess(e.target.value)}
+                required
+              >
+                <MenuItem value="admin">Admin</MenuItem>
+                <MenuItem value="manager">Manager</MenuItem>
+                <MenuItem value="user">User</MenuItem>
+              </Select>
+            </FormControl>
+
+            <Button type="submit" variant="contained" color="primary">
+              Add User
+            </Button>
+          </form>
+        )}
         <DataGrid checkboxSelection rows={mockDataTeam} columns={columns} />
       </Box>
     </Box>
